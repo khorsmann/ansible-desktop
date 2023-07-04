e8dcc6581c191c07f58615dbe52a011913184f75
Karsten Horsmann <khorsmann@gmail.com>
2023-07-04 15:23:12 +0200

    polish setup-my-ansible.sh a bit

--- setup-my-ansible.sh
+++ setup-my-ansible.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
-which pyenv || bash install-pyenv.sh
-which ansible || bash setup-local-ansible.sh
+which git >/dev/null || (echo "Please install 'git'. Abort." && exit 1)
+which pyenv >/dev/null || bash install-pyenv.sh
+which ansible >/dev/null || bash setup-local-ansible.sh
 ansible-galaxy role install -r requirements.yml  --force
 ansible-galaxy collection install -r requirements.yml --force
 touch collections/ansible_collections/.gitdummy
